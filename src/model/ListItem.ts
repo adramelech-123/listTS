b14e4763830d4955c3a08b7625b566802d88c0aa
adramelech-123 <tmamvuto6@gmail.com>
2024-02-23 10:50:30 +0200

    Git Attributes update

--- src/model/ListItem.ts
+++ src/model/ListItem.ts
@@ -5,19 +5,35 @@ export interface Item {
 }
 
 export default class ListItem implements Item {
-    constructor(
-        private _id: string = '',
-        private _item: string = '',
-        private _checked: boolean = false
-    ) {
-        // No need for assigning default values because we have done that in the constructor
-    }
+  constructor(
+    private _id: string = "",
+    private _item: string = "",
+    private _checked: boolean = false
+  ) {
+    // No need for assigning default values because we have done that in the constructor
+  }
 
-    get id(): string { 
-        return this._id;
-    }
+  get id(): string {
+    return this._id;
+  }
 
-    set id(id: string) {
-        this._id = id;
-    }
+  set id(id: string) {
+    this._id = id;
+  }
+
+  get item(): string {
+    return this._item;
+  }
+
+  set item(item: string) {
+    this._item = item;
+  }
+
+  get checked(): boolean {
+    return this._checked;
+  }
+
+  set checked(checked: boolean) {
+    this._checked = checked;
+  }
 }
