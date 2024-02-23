39c26f694a81060552ec851f0d363f02809e066e
adramelech-123 <tmamvuto6@gmail.com>
2024-02-23 22:58:28 +0200

    List template render and app initialization in the mainTs file

--- src/templates/ListTemplate.ts
+++ src/templates/ListTemplate.ts
@@ -9,6 +9,8 @@ interface DOMList {
 export default class ListTemplate implements DOMList {
     ul: HTMLUListElement
 
+    static instance: ListTemplate = new ListTemplate();
+
     private constructor() {
         this.ul = document.getElementById("listItems") as HTMLUListElement;
     }
@@ -20,8 +22,11 @@ export default class ListTemplate implements DOMList {
     render(fullList: FullList): void { 
         this.clear()
         fullList.list.forEach(item => {
+            // Render the List Element
             const li = document.createElement("li") as HTMLLIElement
             li.className = "item"
+
+            // Render checkbox and append to the list element
             const check = document.createElement("input") as HTMLInputElement
             check.type = "checkbox"
             check.id = item.id
@@ -32,6 +37,26 @@ export default class ListTemplate implements DOMList {
                 fullList.save()
             })
             li.append(check)
+
+            // Render the Label and append to list item
+            const label = document.createElement("label") as HTMLLabelElement
+            label.htmlFor = item.id
+            label.textContent = item.item
+            li.append(label)
+
+            // Render Button and append to list item
+            const button = document.createElement("button") as HTMLButtonElement
+            button.className = "button"
+            button.textContent = "X"
+            button.addEventListener('click', () => {
+                fullList.removeItem(item.id)
+                this.render(fullList)
+            })
+
+            li.append(button)
+
+            // Append List Item to the unordered list
+            this.ul.append(li)
         })
     }
 }
